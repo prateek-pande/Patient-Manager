4b2e099662a9813fec8c0efe532ec3a02c06cbe2
prateek.pande <prateek.550@gmail.com>
2016-06-28 19:02:27 +0530

    working on add appointment

--- app/src/main/java/com/example/prateekpande/patientmanager/fragment/AddAppointment.java
+++ app/src/main/java/com/example/prateekpande/patientmanager/fragment/AddAppointment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import com.example.prateekpande.patientmanager.HomeActivity;
 import com.example.prateekpande.patientmanager.R;
 import com.example.prateekpande.patientmanager.model.Appointment;
 
@@ -23,6 +26,7 @@ public class AddAppointment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private Spinner clinicLocations;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -64,6 +68,10 @@ public class AddAppointment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        clinicLocations = (Spinner) getView().findViewById(R.id.spinnerClinic);
+        //initialize form data
+//        initializeFormData();
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_apointment, container, false);
     }
@@ -83,6 +91,14 @@ public class AddAppointment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    public void initializeFormData(){
+
+        //clinic location
+        String[] clinics ={"A","B"};
+        clinicLocations.setAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_dropdown_item,clinics));
+
     }
 
     /**
